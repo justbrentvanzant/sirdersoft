40c38da7c8855fa3f9f4e08ed5dfcbd8a4247aa6
bvanzant <justbrentvanzant@gmail.com>
2020-08-25 21:06:04 -0500

    more buttons

--- driver.js
+++ driver.js
@@ -1,2 +1,19 @@
 //Driver Script
 particlesJS.load('particles-js', 'girderSoft.json');
+window.onload = function(e){ 
+    const demoDownloadButton = document.getElementById("demoDownloadButton");
+    const downloadRow = document.getElementById("downloadRow");
+    demoDownloadButton.onclick = function() { 
+        makeInactive(demoDownloadButton);
+        makeVisible(downloadRow);
+    };
+
+    const makeInactive = function(elementRef) {
+        elementRef.classList.add('inactive');
+    }
+
+    const makeVisible = function(elementRef) {
+        elementRef.classList.add('visible');
+    }
+
+}
